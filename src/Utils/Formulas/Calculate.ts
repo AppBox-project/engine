a21bb56d02c934936724866cf04b15ceacf7f2d4
Vic van Cooten <duveaux1@gmail.com>
2020-08-13 13:09:58 +0200

    Support foreign depedencies

--- src/Utils/Formulas/Calculate.ts
+++ src/Utils/Formulas/Calculate.ts
@@ -1,7 +1,8 @@
 import { systemLog } from "../General";
 import { AutomationContextType } from "../Types";
 var nunjucks = require("nunjucks");
-import { format, formatDistance, formatRelative, subDays } from "date-fns";
+import { format } from "date-fns";
+import { set } from "lodash";
 
 let env = nunjucks.configure();
 env.addFilter("date", (date, dateFormat) => {
@@ -14,11 +15,85 @@ env.addFilter("years", (time) => {
 // Recalculate a formula
 const calculate = async (context: AutomationContextType) => {
   if (context.trigger === "change") {
-    systemLog("Formula recalculation triggered by change.");
+    // Local change
+    // A change in a formula that only references itself and can therefore only update itself.
+    systemLog(
+      `Local change in object ${context.object._id}. Recalculating formula ${context.id.id}.`
+    );
     parseFormula(context.id, context.object, context.model, context.models);
   } else if (context.trigger === "foreignChange") {
-    systemLog("Foreign change occured");
-    console.log(context.id.id);
+    // A foreign change is a change that happened on a formula value with a relationship model. This can impact many objects.
+    // Todo: currently recalculates all objects. In theory it should be possible to backtrace this to only the affected objects. This could be difficult.
+    systemLog(
+      `Foreign change in object ${context.object._id}. Recalculating formula ${context.id.id}.`
+    );
+    const modelId = context.id.id.split(".")[1];
+    const fieldId = context.id.id.split(".")[2];
+
+    // Todo: how to deal with a formula with multiple dependencies of which one foreign?
+    // --> 1. Obtain all models that will be relevant for formula parsing
+    // Do this once, so we don't have to do it per object. It will always be the same.
+    const formulaModels = {};
+    await context.id.originalDependency
+      .split(".")
+      .reduce(async (promise, dependencyPart) => {
+        const model = await promise;
+
+        formulaModels[model.key] = model;
+
+        let modelId;
+        if (dependencyPart.match("_r")) {
+          modelId =
+            model.fields[dependencyPart.replace("_r", "")].typeArgs
+              .relationshipTo;
+        }
+
+        return await context.models.objects.model.findOne({ key: modelId });
+      }, await context.models.objects.model.findOne({ key: modelId }));
+
+    // --> 2. (needs improvement) loop through all objects and recalculate their formulas.
+    context.models.entries.model.find({ objectId: modelId }).then((objects) => {
+      objects.map(async (object) => {
+        // For every object, split the formula
+        const inf = await context.id.originalDependency
+          .split(".")
+          .reduce(async (promise, dependencyPart) => {
+            const object = await promise;
+
+            let objectId;
+            if (dependencyPart.match("_r")) {
+              return await context.models.entries.model.findOne({
+                _id: object.data[dependencyPart.replace("_r", "")],
+              });
+            } else {
+              return object.data[dependencyPart.replace("_r", "")];
+            }
+          }, await context.models.entries.model.findOne({ _id: object._id }));
+
+        // We now have the value in inf
+        // Create data model for nunjucks
+        const data = {
+          TODAY: new Date(),
+        }; // default constants
+        set(data, context.id.originalDependency, inf);
+
+        const model = await context.models.objects.model.findOne({
+          key: modelId,
+        });
+        const newObject = await context.models.entries.model.findOne({
+          _id: object._id,
+        });
+
+        var parsedFormula = env.renderString(
+          model?.fields[fieldId]?.typeArgs?.formula || "Error: formula missing",
+          data
+        );
+
+        newObject.data[fieldId] = parsedFormula;
+        newObject.markModified(`data.${fieldId}`);
+        newObject.save();
+      });
+    });
   } else {
     // Time trigger
     systemLog("Formula recalculation triggered by time.");
@@ -36,7 +111,6 @@ const parseFormula = async (id, object, model, models) => {
   const fieldId = id.id.split(".")[2];
   const field = model.fields[fieldId];
 
-  // Todo: remote data
   // Create data model for nunjucks
   const data = { TODAY: new Date() }; // default constants
 
