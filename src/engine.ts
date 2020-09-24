1cf4128b02938f606704f051916536aa2d70b112
Vic van Cooten <duveaux1@gmail.com>
2020-09-24 11:18:40 +0200

    Remove DB crash (server does this)

--- src/engine.ts
+++ src/engine.ts
@@ -55,7 +55,6 @@ mongoose.connect(
   }
 );
 var db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", async function () {
   models = {
     objects: {
